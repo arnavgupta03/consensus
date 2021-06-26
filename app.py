4a4c9c38c3d651914cd96b9b41acf28321e42a61
arnavgupta03 <arnavgupt03@gmail.com>
2021-06-25 23:58:46 -0400

    Add navigation to page 3

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, session
-import requests
+import requests, os
 from flask_socketio import SocketIO, send, emit, join_room
+from werkzeug import debug
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret"
 socketio = SocketIO(app, cors_allowed_origins="*")
+
+TMDB_KEY = os.environ.get("TMDB_KEY")
 
 @app.route("/")
 def home():
@@ -16,9 +19,15 @@ def test_connect():
 
 @socketio.on("join")
 def on_join(room):
-    session["room"] = room
+    session["roomName"] = room
     join_room(room)
     print("joined " + room)
 
+@socketio.on("page3")
+def page3():
+    room = session.get("roomName")
+    print("got here " + room)
+    emit("switchPageThree", to=room)
+
 if __name__ == "__main__":
-    socketio.run(app)
+    socketio.run(app, debug = True)
--- static/main.js
+++ static/main.js
@@ -1,7 +1,16 @@
 function onLoad() {
     var socket = io.connect("http://127.0.0.1:5000");
-    socket.on("connect", function() {
+    socket.onconnect(function() {
         socket.emit("connect");
+    });
+
+    socket.on("switchPageThree", function() {
+        document.getElementById("page2").remove();
+        var p3title = document.createElement("h3");
+        p3title.id = "p3title";
+        var p3titleTextNode = document.createTextNode("Alright, let's start by choosing the genre(s)!");
+        p3title.appendChild(p3titleTextNode);
+        document.getElementById("page3").appendChild(p3title);
     });
 
     document.getElementById("p1joinCreateGroup").addEventListener("click", function() {
@@ -14,6 +23,9 @@ function onLoad() {
         p2title.appendChild(p2titleTextNode);
         var p2start = document.createElement("button");
         p2start.id = "p2start";
+        p2start.addEventListener("click", function() {
+            socket.emit("page3");
+        });
         var p2startTextNode = document.createTextNode("Start Choosing");
         p2start.appendChild(p2startTextNode);
         document.getElementById("page2").appendChild(p2title);

ae1844821de323dca6833335494551fd0deeb1a6
arnavgupta03 <arnavgupt03@gmail.com>
2021-06-26 10:44:32 -0400

    Add basic page 4 info with details for film card

--- app.py
+++ app.py
@@ -29,7 +29,6 @@ def on_join(room):
 @socketio.on("page3")
 def page3():
     room = session.get("roomName")
-    print("got here " + room)
     genres = requests.get("https://api.themoviedb.org/3/genre/movie/list?api_key=" + TMDB_KEY + "&language=en-US").json()
     session["genres"] = []
     emit("switchPageThree", genres, to=room)
@@ -46,10 +45,21 @@ def genreSelect(data):
     session["genres"] = chosen
     session.modified = True
 
-    print(session.get("genres"))
-
     room = session.get("roomName")
     emit("updateGenres", data, to=room)
+
+@socketio.on("page4")
+def page4():
+    chosenGenres = session.get("genres")
+    if len(chosenGenres) == 0:
+        emit("addMoreGenres")
+    else:
+        genreString = ",".join([str(genre) for genre in chosenGenres])
+        data = requests.get("https://api.themoviedb.org/3/discover/movie?api_key=" + TMDB_KEY + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=" + genreString).json()
+        if len(data["results"]) == 0:
+            emit("chooseDifferentGenres")
+        else:
+            emit("startSelect", {"title": data["results"][0]["title"], "overview": data["results"][0]["overview"], "poster_path": data["results"][0]["poster_path"], "rating": data["results"][0]["vote_average"]})
 
 
 if __name__ == "__main__":
--- static/main.js
+++ static/main.js
@@ -1,7 +1,51 @@
 function onLoad() {
     var socket = io.connect("http://127.0.0.1:5000");
-    socket.onconnect(function() {
+    socket.onconnect(() => {
         socket.emit("connect");
+    });
+
+    socket.on("startSelect", function(filmInfo) {
+        document.getElementById("page3").remove();
+
+        var p4instruct = document.createElement("h3");
+        p4instruct.id = "p4instruct";
+        var p4instructTextNode = document.createTextNode("Now we're going to give you some movies! Tell us if you'd want to watch it or not.");
+        p4instruct.appendChild(p4instructTextNode);
+        document.getElementById("page4").appendChild(p4instruct);
+
+        var p4movieCard = document.createElement("div");
+
+        var p4title = document.createElement("h3");
+        p4title.id = "p4title";
+        var p4titleTextNode = document.createTextNode(filmInfo["title"]);
+        p4title.appendChild(p4titleTextNode);
+        p4movieCard.appendChild(p4title);
+
+        var p4descript = document.createElement("p");
+        p4descript.id = "p4descript";
+        var p4descriptTextNode = document.createTextNode(filmInfo["overview"]);
+        p4descript.appendChild(p4descriptTextNode);
+        p4movieCard.appendChild(p4descript);
+
+        var p4rating = document.createElement("h4");
+        p4rating.id = "p4rating";
+        var p4ratingTextNode = document.createTextNode(filmInfo["rating"]);
+        p4rating.appendChild(p4ratingTextNode);
+        p4movieCard.appendChild(p4rating);
+
+        var p4poster = document.createElement("img");
+        p4poster.src = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/" + filmInfo["poster_path"];
+        p4movieCard.appendChild(p4poster);
+
+        document.getElementById("page4").appendChild(p4movieCard);
+    });
+
+    socket.on("chooseDifferentGenres", () => {
+        alert("No movies available for this genre combination.");
+    });
+
+    socket.on("addMoreGenres", () => {
+        alert("Please add at least one genre");
     });
 
     socket.on("updateGenres", function(genres) {
@@ -15,7 +59,7 @@ function onLoad() {
 
         var p3title = document.createElement("h3");
         p3title.id = "p3title";
-        var p3titleTextNode = document.createTextNode("Alright, let's start by choosing the genre(s)! Only hit Submit once everyone's agreed on which genres to watch.");
+        var p3titleTextNode = document.createTextNode("Alright, let's start by choosing the genre(s)! Only hit Submit once everyone's agreed on which genre/genre combo to watch.");
         p3title.appendChild(p3titleTextNode);
         document.getElementById("page3").appendChild(p3title);
 
@@ -25,7 +69,7 @@ function onLoad() {
             var p3checkbox = document.createElement("input");
             p3checkbox.type = "checkbox";
             p3checkbox.id = "p3checkbox" + i.toString();
-            p3checkbox.addEventListener("click", function() {
+            p3checkbox.addEventListener("click", () => {
                 var checked = [];
                 for (var i = 0; i < 19; i++) {
                     if (document.getElementById("p3checkbox" + i.toString()).checked) {
@@ -47,7 +91,7 @@ function onLoad() {
         p3button.id = "p3button";
         var p3buttonTextNode = document.createTextNode("Submit Genres");
         p3button.appendChild(p3buttonTextNode);
-        p3button.addEventListener("click", function() {
+        p3button.addEventListener("click", () => {
             socket.emit("page4");
         });
         document.getElementById("page3").appendChild(p3checkboxGroup);
@@ -67,7 +111,7 @@ function onLoad() {
 
         var p2start = document.createElement("button");
         p2start.id = "p2start";
-        p2start.addEventListener("click", function() {
+        p2start.addEventListener("click", () => {
             socket.emit("page3");
         });
         var p2startTextNode = document.createTextNode("Start Choosing");

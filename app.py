4e514ac5352c5e5dd676868787ff01ff47851253
arnavgupta03 <arnavgupt03@gmail.com>
2021-06-25 21:38:07 -0400

    Setup basic connection test

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, session
+import requests
+from flask_socketio import SocketIO, send, emit, join_room
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret"
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@socketio.on("connected")
+def test_connect(info):
+    print(info)
+
+if __name__ == "__main__":
+    socketio.run(app)
